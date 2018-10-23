334f213fcc4b282a99c26c31558492319bc3b25a
duongtdn <duongtdn@gmail.com>
2018-10-23 15:34:02 +0000

    add send email

--- lambda.js
+++ lambda.js
@@ -1,5 +1,50 @@
 "use strict"
 
+const banks = {
+  tcb: {
+    name: 'Techcombank HCMC',
+    account: {
+      name: 'Expiup Inc',
+      number: '1111-2222-3333-4455'
+    } 
+  },
+  vcb: {
+    name: 'Vietcombank HCMC',
+    account: {
+      ame: 'Expiup Inc',
+      number: '6666-7777-8888-9900'
+    }
+  }
+}
+
+const aws = require('aws-sdk');
+const lambda = new aws.Lambda();
+
+function sendEmailFn(functionName) {
+  return function sendEmail({recipient, customer, invoice}, callback) {
+
+    console.log(`Sending email to ${recipient}`)
+
+    lambda.invoke(
+      {
+        FunctionName: functionName,
+        InvocationType: "Event",
+        Payload: JSON.stringify({recipient, customer, invoice, banks}, null, 2)
+      },
+      function(err, data) {
+        if (err) {
+          console.log(err)
+          callback(err)
+        } else {
+          console.log('Send Email success')
+          callback()
+        }
+      }
+    )
+
+  }
+}
+
 /* create api */  
 
 const api = {
@@ -20,6 +65,8 @@ enroll.use(require('@sglearn/enrolldb-dynamodb-driver')());
 api.purchase.useDatabase({ invoice, course, enroll })
 api.enroll.useDatabase({ enroll, invoice })
 api.course.useDatabase({ course })
+
+api.purchase.helper({ sendEmail: sendEmailFn('SendEmailPurchaseOrder') })
 
 /* create express app from api */  
 const express = require('express')
